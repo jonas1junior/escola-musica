5a587ce9e9168b11fb8acc57080f8a66898aa3ff
Casseb <felipe.casseb@gmail.com>
2015-10-30 17:44:15 -0200

    Adicionados campos Tipo do curso no formulário e botão de salvar, por enquanto somente com a mensagem "salvo com sucesso"
    Concluído Aula 3: https://www.youtube.com/watch?v=1pvzuiU_EWc

--- src/escola/musica/bean/CursoBean.java
+++ src/escola/musica/bean/CursoBean.java
@@ -1,14 +1,29 @@
 package escola.musica.bean;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
 import escola.musica.modelo.Curso;
+import escola.musica.modelo.TipoCurso;
 
 @ManagedBean
 public class CursoBean {
 
 	//Instanciando um novo curso
 	private Curso curso = new Curso();
+	
+	//Criando tipos que será uma lista com os tipos listados no enumTipoCurso
+	//Arrays.asList esta convertendo um array de tipos de cursos em lista
+	private List<TipoCurso> tipos = Arrays.asList(TipoCurso.values());
 
+	//Método salvar, por enquanto só apresenta a mensagem "Curso salvo com Sucesso"
+	public void salvar(){
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Curso salvo com sucesso"));
+	}
 	
 	//Getters e Setters
 	public Curso getCurso() {
@@ -18,5 +33,15 @@ public class CursoBean {
 	public void setCurso(Curso curso) {
 		this.curso = curso;
 	}
+
+	public List<TipoCurso> getTipos() {
+		return tipos;
+	}
+
+	public void setTipos(List<TipoCurso> tipos) {
+		this.tipos = tipos;
+	}
+	
+	
 
 }
